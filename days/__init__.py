2bcc01bdb964d07a295dbdadcf67568cfa2e5a20
Braden Kinard <bkinard9@gmail.com>
2024-12-06 12:43:41 -0500

    day 2 part 1 solution

--- days/__init__.py
+++ days/__init__.py
@@ -1,5 +1,7 @@
 from .day_1 import solve as solve_day_1
+from .day_2 import solve as solve_day_2
 
 __all__ = [
     "solve_day_1",
+    "solve_day_2",
 ]
---
+++ days/day_2/tests/test_report_is_safe.py
@@ -0,0 +1,36 @@
+from days.day_2.solution import report_is_safe
+
+
+def test_increasing_is_safe() -> None:
+    report = [0, 1, 3, 6, 7]
+    assert report_is_safe(report)
+
+
+def test_decreasing_is_safe() -> None:
+    report = [7, 6, 3, 2, 0]
+    assert report_is_safe(report)
+
+
+def test_increase_too_much() -> None:
+    report = [0, 1, 2, 7]
+    assert not report_is_safe(report, max_diff=3)
+
+
+def test_decrease_too_much() -> None:
+    report = [7, 6, 1, 0]
+    assert not report_is_safe(report, max_diff=3)
+
+
+def test_unsafe_increase_then_decrease() -> None:
+    report = [0, 1, 2, 1]
+    assert not report_is_safe(report)
+
+
+def test_unsafe_decrease_then_increase() -> None:
+    report = [2, 1, 0, 1]
+    assert not report_is_safe(report)
+
+
+def test_unsafe_change_by_0() -> None:
+    report = [0, 0, 1, 2]
+    assert not report_is_safe(report)
